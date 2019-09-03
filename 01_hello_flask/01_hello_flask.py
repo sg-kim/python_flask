6621a6d5b1554be4cce8eb765f9a52b16b151118
Seunggu Kim <sgkim08@gmail.com>
2019-09-03 22:31:23 +0900

    Using Jinja2 template engine w/ Bootstrap CSS

--- 01_hello_flask/01_hello_flask.py
+++ 01_hello_flask/01_hello_flask.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)	# name is a special variable that gets module name
 
 @app.route("/")
+@app.route("/home")
 def hello():
-
 	return "<h1>Hello Flask!</h1>"
 
 if __name__ == "__main__":
