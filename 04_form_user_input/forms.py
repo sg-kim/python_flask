3ffc281b66d0e04157d9b963ec433e67a0889d17
Seunggu Kim <sgkim08@gmail.com>
2019-09-04 19:35:44 +0900

    exercise on form and user input

---
+++ 04_form_user_input/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):	#	this class is inherited from FlaskForm
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])	#	instantiate StringField class
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):	#	this class is inherited from FlaskForm
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
+
