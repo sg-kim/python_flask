6d77add02b3d115710a2254389afd0e4c278abb9
Seunggu Kim <sgkim08@gmail.com>
2019-09-08 08:45:44 +0900

    exercise on implementing user account profile and picture

--- 08_user_account_profile_picture/DBwithFlaskPackage/forms.py
+++ 08_user_account_profile_picture/DBwithFlaskPackage/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -30,8 +31,7 @@ class LoginForm(FlaskForm):	#	this class is inherited from FlaskForm
 class UpdateAccountForm(FlaskForm):	#	this class is inherited from FlaskForm
 	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])	#	instantiate StringField class
 	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
 	submit = SubmitField('Update')
 
 	def validate_username(self, username):
