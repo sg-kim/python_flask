dfa0064b2b04267e1aa3b0d4f37ffc86e89a6100
Seunggu Kim <sgkim08@gmail.com>
2019-09-14 10:49:10 +0900

    Python with MySQL

--- 10_DB_with_MySQL/DBwithMySQL/__init__.py
+++ 10_DB_with_MySQL/DBwithMySQL/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://engineer:heybuddy01@localhost:3306/flask_test_db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://engineer:heybuddy01@localhost:3306/flask_test_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://engineer:heybuddy01@localhost/flask_test_db'
 db = SQLAlchemy(app)
 
