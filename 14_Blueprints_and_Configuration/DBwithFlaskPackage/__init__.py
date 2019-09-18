285a0dec742599ddeca5c9db14a21e217c08c663
Seunggu Kim <sgkim08@gmail.com>
2019-09-18 20:08:29 +0900

    Exercise on restructuring an App with Blueprint and Congig class

---
+++ 14_Blueprints_and_Configuration/DBwithFlaskPackage/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from DBwithFlaskPackage.config import Config
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+def create_app(config_class = Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
+	
+	db.init_app(app)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
+
+	from DBwithFlaskPackage.users.routes import users
+	from DBwithFlaskPackage.posts.routes import posts
+	from DBwithFlaskPackage.main.routes import main 
+	
+	app.register_blueprint(users)
+	app.register_blueprint(posts)
+	app.register_blueprint(main)
+
+	return app
+
---
+++ 14_Blueprints_and_Configuration/run.py
@@ -0,0 +1,7 @@
+from DBwithFlaskPackage import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+	app.run('172.18.2.46')
+
